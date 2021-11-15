74b57c316ebb439761e1579667748f3e9617d0d2
Gilberto Santamaría <gilbertosa099@outlook.com>
2021-11-15 17:26:08 -0500

    [ADD] First files

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+//Canvas
+const canvas = document.getElementById("canvas1");
+const context = canvas.getContext("2d");
+
+canvas.width = 400;
+canvas.height = 250;
+
+let score = 0;
+let gameFrame = 0;
+
+context.font = "50px Georgia";
+//Mouse interaction
+//player
+//bubbles
+//animatiosn
